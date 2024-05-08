cf72f1a35aacba14c4703f28a9409e3523fa0dc7
Charles Patterson <61368231+charles-v-patterson@users.noreply.github.com>
2024-05-08 11:36:59 -0500

    updated dependancies and fixed middleware authentication error.

--- backend/server.js
+++ backend/server.js
@@ -1,39 +1,46 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const dotenv = require('dotenv'); 
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const dotenv = require("dotenv");
 
 // Import Routes
-const authRoutes = require('./routes/authRoutes'); 
-const checkinRoutes = require('./routes/checkinRoutes');
+const authRoutes = require("./routes/authRoutes");
+const checkinRoutes = require("./routes/checkinRoutes");
 
 // Configure Environment Variables
 dotenv.config();
 
 // Express App Initialization
 const app = express();
-const port = process.env.PORT || 5000; 
+const port = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());  // Enable CORS
+app.use(cors()); // Enable CORS
 app.use(express.json()); // Parse incoming JSON payloads
 
 // Connect to MongoDB
 mongoose
   .connect(process.env.MONGODB_URI)
-  .then(() => console.log('Connected to MongoDB'))
-  .catch(err => console.error('Could not connect to MongoDB', err));
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.error("Could not connect to MongoDB", err));
 
 // API Routes
+<<<<<<< Updated upstream
 app.post('/api/login', authRoutes);
 app.post('/api/passwordReset', authRoutes);
 app.post('/api/register', authRoutes);
 app.post('/api/checkin', checkinRoutes);
 app.get('/api/check-network', checkinRoutes);
+=======
+app.post("/api/login", authRoutes);
+app.post("/api/passwordReset", authRoutes);
+app.post("/api/register", authRoutes);
+app.get("/api/check-network", checkinRoutes);
+>>>>>>> Stashed changes
 
-// Basic Route for Testing 
- app.get('/', (req, res) => {
-  res.send('Employee Check-In Backend');
+// Basic Route for Testing
+app.get("/", (req, res) => {
+  res.send("Employee Check-In Backend");
 });
 
 // Start Server
